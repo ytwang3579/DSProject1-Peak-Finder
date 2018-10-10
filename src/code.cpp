0c4590c46f93afbf14052fed86a1d8c443889bf8
ytwang <ytwang3579@gmail.com>
2018-10-10 16:31:20 +0800

    input process updated

--- src/code.cpp
+++ src/code.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-
+    ofstream outfile("final.peak");
     if(argc != 2){
         cout << "Input Error!!\n";
         return 1;
@@ -23,21 +23,25 @@ int main(int argc, char* argv[])
 
 // set edges to be INT32_MIN
     for(int i=0; i<=m+1; i++){
-        matrix[i][0] = matrix[0][i] = INT32_MIN;
+        matrix[i][0] = matrix[i][n+1] = INT32_MIN;
     }
+    for(int i=1; i<=n; i++){
+        matrix[0][i] = matrix[m+1][i] = INT32_MIN;
+    }
+
 // read input to matrix
-    for(int i=1; i++; i<=m){
-        for(int j=1; j++; j<=n){
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
             infile >> matrix[i][j];
         }
     }
 
 // print matrix
-    for(int i=1; i++; i<=m){
-        for(int j=1; j++; j<=m){
-            cout << matrix[i][j] << " ";
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            outfile << matrix[i][j] << " ";
         }
-        cout << '\n';
+        outfile << '\n';        
     }
 
     return 0;
